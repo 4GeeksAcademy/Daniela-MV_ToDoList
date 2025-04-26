1d12a918b240bc8b6201eb3d93333c25ec460fbe
Daniela-MV <danmv.0991@gmail.com>
2025-04-26 17:51:20 +0000

    Botón de borrar

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -25,22 +25,33 @@ const Home = () => {
 	}
 
 	return (
+
+// 		<div class="accordion accordion-flush" id="accordionFlushExample">
+//   <div class="accordion-item">
+//     <h2 class="accordion-header">
+//       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+//         Accordion Item #1
+//       </button>
+//     </h2>
 	
-		<div className="d-flex justify-content-center" style={{backgroundColor: "#f0f0f0", minHeight: "100vh"}} >
-			<div className="form text-center fs-1 mt-5" style={{width: "900px"}}>
-			<label type="text">Todos</label>
-				<input 
-				type="text" 
-				className="form-control" 
-				value={nuevaTarea}  
-				placeholder="Añade una tarea" 
-				onChange={entrada} 	
-				onKeyDown={inputEnter}/>	
+	 <div className="d-flex justify-content-center" style={{backgroundColor: "#f0f0f0", minHeight: "100vh"}} >
+	 	<div className="form text-center fs-1 mt-5" style={{width: "900px"}}>
+	 	<label type="text">Todos</label>
+	 		<input 
+	 		type="text" 
+	 		className="form-control" 
+	 		value={nuevaTarea}  
+	 		placeholder="Añade una tarea" 
+	 		onChange={entrada} 	
+	 		onKeyDown={inputEnter}/>	
 
                 <ul>
 				{tareas.map((tarea, index)=> 
 				<li key={index}> 
 				<span className="text">{tarea}</span>
+				<button type="button" class="btn btn-light">
+				<i className="bi bi-clipboard-x"></i>
+				</button>
 				</li>)}
 			    </ul>
 			</div>
